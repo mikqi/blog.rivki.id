69ab138360f01c76d4834c65b765469543a34844
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-05-26 03:41:06 +0700

    add image formating

--- src/components/PostFormatting/PostFormatting.jsx
+++ src/components/PostFormatting/PostFormatting.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import "./PostFormatting.css";
 
 class PostFormatting extends React.Component {
+  componentDidMount() {
+    const elements = document.querySelectorAll('img[src$="#forty-percent"]')
+    elements.forEach(element => {
+      element.parentElement.addClass('u-align-center')
+    })
+  }
+
   render() {
     const { children, className } = this.props;
     return <article className={className}>{children}</article>;
