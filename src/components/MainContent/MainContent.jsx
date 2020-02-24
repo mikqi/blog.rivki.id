251569e5bfb2ebd355e508d590c788c2ce4635f8
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-02-24 15:00:37 +0700

    support reading mode

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import classNames from "classnames";
-import "./MainContent.css";
+import React from 'react'
+import classNames from 'classnames'
+import './MainContent.css'
 
 class MainContent extends React.Component {
   render() {
-    const { children } = this.props;
+    const { children } = this.props
 
-    const classes = classNames("content", this.props.className);
+    const classes = classNames('content', this.props.className)
 
     return (
-      <main id="content" className={classes} role="main">
+      <article id="content" className={classes}>
         {children}
-      </main>
-    );
+      </article>
+    )
   }
 }
 
-export default MainContent;
+export default MainContent
