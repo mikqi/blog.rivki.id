7ab28eec94088dd9705ad5b7bfd584a84ee89ca4
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-03-01 02:34:24 +0700

    add 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,31 @@
-import React, { Component } from "react";
-import Helmet from "react-helmet";
-import config from "../../data/SiteConfig";
+import React, { Component } from 'react'
+import { navigate } from 'gatsby'
+import Helmet from 'react-helmet'
+import config from '../../data/SiteConfig'
 
 class AboutPage extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      navigate('/')
+    }, 30000)
+  }
+
   render() {
     return (
       <div className="about-container">
         <Helmet title={`About | ${config.siteTitle}`} />
-        <h1>404 Page</h1>
+        <div
+          className="shakeitoff"
+          style={{
+            height: '100vh',
+            width: '100vw',
+            background: 'url(/images/taken.png) no-repeat center center'
+          }}
+        />
+        {/* <img src="/images/taken.png" alt="not found" /> */}
       </div>
-    );
+    )
   }
 }
 
-export default AboutPage;
+export default AboutPage
