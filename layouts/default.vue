3f9207f7585fdb41f78f1e51012f260cb402aee3
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-09-20 19:28:46 +0700

    feat: update layout page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
     <Header />
-    <div class="container max-w-2xl mx-auto">
+    <div class="min-container container max-w-2xl mx-auto">
       <Nuxt />
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
 export default {
   name: 'DefaultLayout',
   components: {
     Header,
+    Footer,
   },
 }
 </script>
+
+<style scoped>
+.min-container {
+  min-height: calc(100vh - 400px);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Hello</h1>
+  <div class="prose prose-sm md:prose-lg xl:prose-xl px-6 sm:px-0">
+    <h4>Hello.</h4>
+    <h2 class="font-normal">I'm Muhammad Rivki</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+      accusantium doloribus reprehenderit impedit vitae quis illum reiciendis
+      optio provident sit nisi, est autem <code>voluptatibus</code> ea cumque.
+      Optio quos illum totam.
+    </p>
+  </div>
 </template>
 
 <script>
