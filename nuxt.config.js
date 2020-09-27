a2e4dc3a5279004fecd07126311f5c8c57853bd8
Muhammad Rivki <muhammad.rivki@live.com>
2020-09-28 00:28:20 +0700

    featt: update about page content

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
   /*
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,31 @@
 <template>
-  <h1>About Page</h1>
+  <div class="prose prose-xl pb-24">
+    <div class="flex justify-center align-middle">
+      <img
+        class="rounded-full w-1/4"
+        src="https://this.rivki.id/static/ava-efd0a0ab46f0651758be5062bce5ebb8.jpg"
+        alt="avatar"
+      />
+      <h3 class="flex self-start md:self-center flex-col ml-4 mt-4">
+        Hello.
+        <span class="m-0">
+          <em>—</em>
+        </span>
+      </h3>
+    </div>
+    <p>
+      I am Rivki, but my friend sometimes call me Ikki (e'ki). A Frontend
+      Engineer who love simplicity, great performance in the good looking
+      products that I made. Previously I am working at the most growing
+      e-commerce company in Indonesia, Bukalapak.
+    </p>
+
+    <p>
+      My passionate is about web technology such as Javascript, Chrome Developer
+      Tools, React, React Native, VueJS and many more. If you want to know more
+      about me just say "Hi!" here.
+    </p>
+  </div>
 </template>
 
 <script>
